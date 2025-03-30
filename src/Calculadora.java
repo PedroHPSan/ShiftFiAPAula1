9fb1238567e7ff995a2140ffb9ddca0c1eb2f534
Pedro Santos <pedropalhetasantos@gmail.com>
2025-03-30 17:52:02 -0300

    Refazendo os modulos basicos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,27 +3,29 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int valor1, valor2;
-        System.out.println("Insira o primeiro valor: ");
+        Scanner leitor = new Scanner(System.in);
 
-        valor1 = sc.nextInt();
+        int num1, num2;
 
-        System.out.println("insira o segundo valor: ");
+        System.out.println("Insira o primeiro numero: ");
+        num1 = leitor.nextInt();
 
-        valor2 = sc.nextInt();
+        System.out.println(" insira o segundo numero: ");
+        num2 = leitor.nextInt();
 
-        int soma = valor1+valor2;
-        int sub = valor1-valor2;
-        double div = ( double) valor1/( double) valor2;
-        int mult = valor1*valor2;
+        //processamento
 
-        System.out.println("Segue o resultado da soma dos valores: " + soma);
-        System.out.println("segue o resultado da Subtração dos valores: " + sub);
-        System.out.println("segue o resultado da Divisão dos valores: " + div);
-        System.out.println("segue o resultado da Multiplicação dos valores: " + mult);
+        int soma = num1 + num2;
+        int subtracao = num1 - num2;
+        int multiplicacao = num1 * num2;
+        // Isso é um cast
+        double divisao = (double)num1 / num2;
 
 
-        sc.close();
+        System.out.println("O resultado das operações são: " + soma
+                            + " é o resultado da soma, " + subtracao
+                            + " é o resultado da subtração, " + multiplicacao
+                            + " é o resultado da multiplicação, " + divisao
+                            + " é o resultado da divisão.");
     }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,22 +3,24 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
+        // Conceito basico de programação são 3 comandos Entrada - saida e processamento
+        // .Jar é o Bytecode ou seja é o arquivo binario
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Hello World");
+        System.out.println("boa tarde! \nVamos pegar essa certificação e melhorar nosso currículo Pedro!!!! ");
 
-
-
-        //Variável de memória -> espaço na memoria RAM que guarda uma informação
-
+        //Variável de memória -> espaço na memória RAM que guarda uma informação
         String name;
         int idade;
         System.out.println("Digite seu nome: ");
         name = sc.nextLine(); //leitura de uma string no terminal
-        System.out.println("Boa noite! " + name + "\nbem vindo ao Shift java!!");
+        System.out.println("Boa noite! " + name + "\nbem vindo ao Shift FIAP java Expert!!");
 
         System.out.println("Digite sua idade: ");
         idade = sc.nextInt();
-        System.out.println("");
+        System.out.println("" + idade);
     }
 }
